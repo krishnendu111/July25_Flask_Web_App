82761d3907d6fa8ee14f464aa3df7e74928d7905
krishnendu111 <sonukrishnendu@gmail.com>
2025-08-28 23:37:30 +0530

    Added name change from suraaj to krish

--- hello.py
+++ hello.py
@@ -2,13 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/suraaj", methods=['GET'])
+@app.route("/krish", methods=['GET'])
 def hello_world():
-    return "<p>Hello, World- My name is suraaj!</p>"
+    return "<p>Hello, World- My name is krish!</p>"
 
 @app.route("/ping", methods=['GET'])
 def pinger():
-    return {'message': 'suraaj'}
+    return {'message': 'krish'}
 
 
 
